6c9f15a046d5bebaed5b9a9173d71bc96b6dbcef
Ivan Kuliasov <dfyreybr@gmail.com>
2022-12-05 18:11:24 +0300

    init: create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { writeFile } from "node:fs/promises";
+import fs from "node:fs";
+
 const create = async () => {
-    // Write your code here 
+  const fileText = "I am fresh and young";
+  const srcPath = "src/fs//files/fresh.txt";
+  const errMessage = "FS operation failed";
+
+  fs.access(srcPath, (error) => {
+    try {
+      if (error) {
+        writeFile(srcPath, fileText);
+      } else {
+        throw new Error(errMessage);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  });
 };
 
 await create();
