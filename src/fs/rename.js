dae091dd8c965170218d56232fd08a0ca7d06e13
Ivan Kuliasov <dfyreybr@gmail.com>
2022-12-05 18:35:19 +0300

    init: rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import { existsSync, renameSync } from "node:fs";
+
 const rename = async () => {
-    // Write your code here 
+  const wrongFilePath = "src/fs/files/wrongFilename.txt";
+  const correctFilePath = "src/fs//files/properFilename.md";
+  const errMessage = "FS operation failed";
+
+  try {
+    if (existsSync(correctFilePath) || !existsSync(wrongFilePath)) {
+      throw new Error(errMessage);
+    }
+    await renameSync(wrongFilePath, correctFilePath);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await rename();
