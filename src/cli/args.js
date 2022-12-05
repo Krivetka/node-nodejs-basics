a4dc12fc46840a5532a67716d58baf05001fba76
Ivan Kuliasov <dfyreybr@gmail.com>
2022-12-05 22:03:53 +0300

    init: args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const result = args
+    .map((arg) => {
+      if (arg.includes("--")) {
+        return `${arg.replace("--", "")} is`;
+      }
+      return `${arg},`;
+    })
+    .join(" ")
+    .slice(0, -1);
+  console.log(result);
 };
 
 parseArgs();
