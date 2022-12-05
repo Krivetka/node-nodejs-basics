50b64756b18e56b5d778e213454a2a5ddea74685
Ivan Kuliasov <dfyreybr@gmail.com>
2022-12-05 23:07:31 +0300

    init: compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { pipeline } from "node:stream";
+import { createGzip } from "node:zlib";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
 const compress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const source = __dirname + "/files/fileToCompress.txt";
+  const destination = __dirname + "/files/archive.gz";
+  const readFileStream = createReadStream(source);
+  const gzip = createGzip();
+  const writeFileStream = createWriteStream(destination);
+  await pipeline(readFileStream, gzip, writeFileStream, (err) => {
+    if (err) {
+      process.stdout.write(errMessage + '\n');
+      process.exitCode = 1;
+    }
+  });
 };
 
 await compress();
