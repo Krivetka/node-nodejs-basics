862c34e228ae5e6cfb306a9e80a2ce11b4033c04
Ivan Kuliasov <dfyreybr@gmail.com>
2022-12-05 22:37:48 +0300

    init: calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { createHash } from 'node:crypto';
+import { readFile } from 'node:fs/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const filePath = __dirname + "/files/fileToCalculateHashFor.txt";
+    const file = await readFile(filePath);
+    const hash = createHash('sha256');
+    const hex = hash.update(file).digest('hex');
+    console.log(hex);
 };
 
 await calculateHash();
