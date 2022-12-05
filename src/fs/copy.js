87c99077ffa71a2c6ec66a86bd7eca09bd8d9886
Ivan Kuliasov <dfyreybr@gmail.com>
2022-12-05 17:00:17 +0300

    init: copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
+import { cp } from "node:fs/promises";
+import { existsSync } from "node:fs";
+
 const copy = async () => {
-    // Write your code here 
+  const srcPath = "src/fs/files";
+  const copyPath = "src/fs//files_copy";
+  const errMessage = "FS operation failed";
+
+  try {
+    if (!existsSync(srcPath) || existsSync(copyPath)) {
+      throw new Error(errMessage);
+    }
+    await cp(srcPath, copyPath, { recursive: true });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await copy();
